f7cf47e95cc7a007d01ddcc496991f6311764f44
dawidkotarba <dawidkotarba>
2021-09-10 22:10:44 +0200

    [TECH] Polonizacja headera i czasu czytania

--- js/readtime.js
+++ js/readtime.js
@@ -1,7 +1,7 @@
 // readtime
 !function (a) {
     a.fn.readtime = function (b) {
-        var c = {"class": "readtime", format: "# min read", images: 12, wpm: 275, wrapper: "time"};
+        var c = {"class": "readtime", format: "czas czytania: # minut", images: 12, wpm: 275, wrapper: "time"};
         return b = a.extend(c, b), this.each(function () {
             var c = this.textContent || this.innerText || "",
                 d = c.replace(/(^\s*)|(\s*$)/gi, "").replace(/[ ]{2,}/gi, " ").replace(/\n /, "\n").split(" ").length,
@@ -16,7 +16,7 @@
 $(document).ready(function () {
     $('.blog-post').readtime({
         class: 'readtime',   // Class name of the output element
-        format: '~ # min read', // # == the number of minutes
+        format: '~ czas czytania: # minut', // # == the number of minutes
         images: 12,           // Seconds per image, false to disable
         wpm: 275,          // Words per minute, defaults to average
         wrapper: 'time'        // What the output will be wrapped in
